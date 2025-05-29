674b8cefc15c455190722bc6ae4f38ccae1b71f9
Yaki <yakovtz@outlook.com>
2025-05-29 22:59:14 +0300

    bug4

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -38,10 +38,10 @@ export default async function LatestInvoices({
                 )}
                 <div className="min-w-0">
                   <p className="truncate text-sm font-semibold md:text-base">
-                    {invoice.customer?.name || invoice.name}
+                    {invoice.customer.name}
                   </p>
                   <p className="hidden text-sm text-gray-500 sm:block">
-                    {invoice.customer?.email || invoice.email}
+                    {invoice.customer.email}
                   </p>
                 </div>
               </div>
