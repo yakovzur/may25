f4b047f1bc5ce89e047fd0afe11901df22eeff60
Yaki <yakovtz@outlook.com>
2025-06-01 18:18:42 +0300

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/tzur.ico" />
       </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
