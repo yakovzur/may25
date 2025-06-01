239b498e65458421839ef231509b7a130d017a2e
Yaki <yakovtz@outlook.com>
2025-06-01 14:10:09 +0300

    1

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,19 +1,10 @@
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchRevenue } from '@/app/lib/data';
 
 export default async function Page() {
   const revenue = await fetchRevenue();
-  const {
-    collectedAmount,
-    pendingAmount,
-    numberOfInvoices,
-    numberOfCustomers,
-  } = await fetchDashboardStats();
-
-  const latestInvoices = await fetchLatestInvoices();
 
   return (
     <main>
@@ -21,14 +12,10 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={`$${collectedAmount.toLocaleString()}`} type="collected" />
-        <Card title="Pending" value={`$${pendingAmount.toLocaleString()}`} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card title="Total Customers" value={numberOfCustomers} type="customers" />
+        <Card title="Revenue" value={`$${revenue.total?.toLocaleString() ?? '0'}`} type="collected" />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue} />
-        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
